4ff783c3827ec59080afdfcfb9c7ea7562083945
k0taro9 <126588189+k0taro9@users.noreply.github.com>
2024-01-15 08:00:13 +0100

    finished fakultaet

--- fakultaet/src/fakultaet/Fakultaet.java
+++ fakultaet/src/fakultaet/Fakultaet.java
@@ -1,9 +1,27 @@
 package fakultaet;
 
 public class Fakultaet {
-
-	
-	public static void main(String[] args) {
-		
-	}
+    
+    static int[] erstelleFakultaetenArray(int groesse) {
+        int[] array = new int[groesse];
+        
+        for (int i = 0; i < groesse; i++) {
+            array[i] = berechneFakultaet(i);
+        }
+        return array;
+    }
+    
+    static int berechneFakultaet(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * berechneFakultaet(n - 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int[] liste = erstelleFakultaetenArray(20);
+        
+        System.out.println("Wert an Index 2: " + liste[2]);
+    }
 }
