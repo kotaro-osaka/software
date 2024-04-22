3d7c30cf4de839840f8812eea33592e4ef571963
Kotaro Osaka <126588189+k0taro9@users.noreply.github.com>
2024-04-22 08:34:25 +0200

    final

---
+++ sorting_algorithms/src/sorting_algorithms/BubbleSort.java
@@ -0,0 +1,31 @@
+package sorting_algorithms;
+
+public class BubbleSort {
+	
+	public static void main(String[] args) {
+		int[] list = { 1, 5, 3, 6, 7, 4, 9, 8, 2 };
+		int temp;
+		
+		for (int item : list) {
+			System.out.print(item);
+		}
+		System.out.println();
+		
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < list.length-i-1; j++) {
+				if (list[j] > list[j+1]) {
+					temp = list[j];
+					list[j] = list[j+1];
+					list[j+1] = temp;
+				}
+			}
+		}
+		
+		for (int item : list) {
+			System.out.print(item);
+		}
+		System.out.println();
+		
+	}
+	
+}
