200589dc8719b969d5ad4b4ac46498dcb56fb8a3
kotaro.osaka <kotaro.osaka@603-PC12.bko.local>
2025-09-05 11:23:36 +0200

    test

--- ue-03-datenbankzugriff/src/datenbank/Test.java
+++ ue-03-datenbankzugriff/src/datenbank/Test.java
@@ -5,6 +5,6 @@ import java.time.LocalDate;
 public class Test {
 	public static void main(String[] args) {
 		// Datenbank.printKundenstamm();
-		Datenbank.insertKunde(5000, "test", "test", LocalDate.of(2001, 1, 01), "test", LocalDate.of(2001, 1, 01), 0, 0, 0, 0);
+		// Datenbank.insertKunde(5000, "test", "test", LocalDate.of(2001, 1, 01), "test", LocalDate.of(2001, 1, 01), 0, 0, 0, 0);
 	}
 }
